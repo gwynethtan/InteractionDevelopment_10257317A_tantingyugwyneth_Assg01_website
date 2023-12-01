d012adc1b0d9078773324e6b3d687ea7d6582352
gwynethtann <S10257317@connect.np.edu.sg>
2023-12-01 11:52:26 +0800

    settled testimonial and footer positioning

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-function purchase(image, item, quantity, price) {
-    this.image = image;
-    this.item = item;
-    this.quantity = quantity;
-    this.price = price;
-    this.purchase_info = function () {
-        let newDiv1 = document.createElement("div");
-        newDiv1.classList.add("mystyle");
-        newDiv1.innerHTML = "Waffle pack <br>";
-        document.body.appendChild(newDiv1);
-    }
-}
+const testimonials = [
+        { content: "Easy to follow instructions and I had a fun time baking it", person: "- Miss Tan" },
+        { content: "Delicious and fluffy! The best recipe I've tried so far.", person: "- Mr. Smith" },
+        { content: "I love the variety of flavors. Great recipe collection!", person: "- Mrs. Johnson" }
+    ];
 
-let waffle = new purchase('Waffle', 0, 6, /* provide the correct price */);
+    let i = 0;
+    //Total Slides
+    let j = testimonials.length;
+    let testimonialContainer = document.getElementById("bread_container");
+    let nextBtn = document.getElementById("next");
+    let prevBtn = document.getElementById("prev");
+    nextBtn.addEventListener("click", () => {
+      i = (j + i + 1) % j;
+      displayTestimonial();
+    });
+    prevBtn.addEventListener("click", () => {
+      i = (j + i - 1) % j;
+      displayTestimonial();
+    });
 
-var w = document.getElementById('waffle_button');
+    let displayTestimonial = () => {
+      testimonialContainer.innerHTML = `
+        <p>${testimonials[i].content}</p>
+        <p>${testimonials[i].person}</>
+      `;
+    };
 
-w.addEventListener('click', function() {
-    waffle.purchase_info();
-}, false);
+
+    window.onload = displayTestimonial;
